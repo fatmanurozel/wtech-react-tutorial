30d55595d04407f11c8d178d1ae4c981a76c6b7a
yakintech <cagatayyildiz87@gmail.com>
2021-11-17 14:58:04 +0300

    jsx map sample

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 
+import JsxSample from './jsxSample/JsxSample'
+import JsxSample2 from './jsxSample/JsxSample2'
+import JsxSample3 from './jsxSample/JsxMapSample'
+import JsxSample4 from './jsxSample/JsxMapSample2'
 import Footer from './template-1/Footer'
 import Header from './template-1/Header'
 import Navbar from './template-1/Navbar'
 import PageContent from './template-1/PageContent'
+import JsxMapSample3 from './jsxSample/JsxMapSample3'
+import JsxMapSample4 from './jsxSample/JsxMapSample4'
+
+
 
 
 function App() {
 
   return (
     <>
-      <Navbar></Navbar>
+      {/* <Navbar></Navbar>
       <Header></Header>
       <PageContent></PageContent>
-      <Footer></Footer>
+      <Footer></Footer> */}
+
+      {/* <JsxSample></JsxSample> */}
+      {/* <JsxSample2></JsxSample2> */}
+      {/* <JsxSample3></JsxSample3> */}
+      {/* <JsxSample4></JsxSample4> */}
+      {/* <JsxMapSample3></JsxMapSample3> */}
+      <JsxMapSample4></JsxMapSample4>
+      
 
     </>
 
